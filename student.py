8c65fafdc3f7efd03b706faeaf965e197ea3d63a
Fernando Christyanto <christyantofernando@gmail.com>
2019-12-09 23:24:44 +0700

    add from_dict to student

--- student.py
+++ student.py
@@ -2,3 +2,9 @@ class Student:
     def __init__(self, name=None, age=None):
         self.name = name
         self.age = age
+
+    @classmethod
+    def from_dict(cls, dict):
+        obj = cls()
+        obj.__dict__.update(dict)
+        return obj
